db6ebd5ed2867e2f39be618dced4226849185d31
Daniele Bonaldo <dany.bony@gmail.com>
2017-08-27 16:59:40 +0200

    Add first arduino to Android Things serial communication

--- AndroidThings/things/src/main/java/net/bonysoft/wordclock/WordClockActivity.kt
+++ AndroidThings/things/src/main/java/net/bonysoft/wordclock/WordClockActivity.kt
@@ -1,7 +1,14 @@
 package net.bonysoft.wordclock
 
 import android.app.Activity
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import com.felhr.usbserial.UsbSerialDevice
+import com.felhr.usbserial.UsbSerialInterface
+import timber.log.Timber
+import java.nio.charset.Charset
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -38,10 +45,43 @@ class WordClockActivity : Activity() {
     override fun onStart() {
         super.onStart()
         configPresenter.startPresenting()
+        doUSBThings()
     }
 
+    private fun doUSBThings() {
+        val usbManager = getSystemService<UsbManager>(UsbManager::class.java)
+        val connectedDevices = usbManager.deviceList
+        for (device in connectedDevices.values) {
+            if (device.getVendorId() === 0x1a86 && device.getProductId() === 0x7523) {
+                Timber.d("Device found: " + device.deviceName)
+                startSerialConnection(usbManager, device)
+                break
+            }
+        }
+    }
+
+    private fun startSerialConnection(usbManager: UsbManager, device: UsbDevice) {
+        val connection = usbManager.openDevice(device)
+        val serial = UsbSerialDevice.createUsbSerialDevice(device, connection)
+
+        if (serial != null && serial.open()) {
+            serial.setBaudRate(115200)
+            serial.setDataBits(UsbSerialInterface.DATA_BITS_8)
+            serial.setStopBits(UsbSerialInterface.STOP_BITS_1)
+            serial.setParity(UsbSerialInterface.PARITY_NONE)
+            serial.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
+            serial.read(object : UsbSerialInterface.UsbReadCallback {
+                override fun onReceivedData(data: ByteArray) {
+                    val dataStr = String(data, Charset.forName("UTF-8"))
+                    Timber.d("Received $dataStr")
+                }
+            })
+        }
+    }
+
+
     override fun onStop() {
-        super.onStop()
         configPresenter.stopPresenting()
+        super.onStop()
     }
 }
