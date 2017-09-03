54f4a11902097c857bf6cd28c9ea337965cbc3f1
Daniele Bonaldo <dany.bony@gmail.com>
2017-09-03 11:57:08 +0200

    Enable “demo” mode with random colors every minute

--- AndroidThings/things/src/main/java/net/bonysoft/wordclock/ArduinoConnectionPresenter.kt
+++ AndroidThings/things/src/main/java/net/bonysoft/wordclock/ArduinoConnectionPresenter.kt
@@ -1,8 +1,12 @@
 package net.bonysoft.wordclock
 
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
-import android.os.Handler
 import com.felhr.usbserial.UsbSerialDevice
 import com.felhr.usbserial.UsbSerialInterface
 import net.bonysoft.wordclock.common.Configuration
@@ -10,24 +14,47 @@ import org.joda.time.DateTimeZone
 import org.joda.time.LocalTime
 import timber.log.Timber
 import java.nio.charset.Charset
+import java.util.*
 
-class ArduinoConnectionPresenter(val usbManager: UsbManager,
+
+class ArduinoConnectionPresenter(val activity: Activity,
+                                 val usbManager: UsbManager,
                                  val matrixDisplayer: MatrixGenerator,
                                  val matrixSerialiser: MatrixSerialiser,
-                                 val handler: Handler,
                                  val configurationPersister: ConfigurationPersister) {
 
-    private var configuration: Configuration = configurationPersister.lastSavedConfiguration()
     private var serialConnection: UsbSerialDevice? = null
 
     fun startPresenting() {
         val connectedDevices = usbManager.deviceList
         for (device in connectedDevices.values) {
-            if (device.getVendorId() === 0x1a86 && device.getProductId() === 0x7523) {
+            if (device.vendorId == 0x1a86 && device.productId == 0x7523) {
                 startSerialConnection(usbManager, device)
                 break
             }
         }
+
+        activity.registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
+        updateDisplay()
+    }
+
+    private val tickReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action.compareTo(Intent.ACTION_TIME_TICK) == 0) {
+                // TODO this is for demo only
+                randomizeConfiguration()
+
+                updateDisplay()
+            }
+        }
+    }
+
+    private val random = Random()
+    private fun randomizeConfiguration() {
+        val colors = activity.resources.getIntArray(R.array.palette_colors)
+        val randomColor = colors[random.nextInt(colors.size)]
+        val newConfiguration = Configuration(randomColor)
+        configurationPersister.saveConfiguration(newConfiguration)
     }
 
     private fun startSerialConnection(usbManager: UsbManager, device: UsbDevice) {
@@ -45,34 +72,23 @@ class ArduinoConnectionPresenter(val usbManager: UsbManager,
                 Timber.d("Received $dataStr")
             }
             updateDisplay()
-            scheduleUpdate()
         }
     }
 
-    private fun scheduleUpdate() {
-        handler.postDelayed(triggerRunnable, 60000)
-    }
-
-    private val triggerRunnable: Runnable = Runnable {
-        updateDisplay()
-        scheduleUpdate()
-    }
-
     fun updateConfiguration(newConfiguration: Configuration) {
-        this.configuration = newConfiguration
-        configurationPersister.saveConfiguration(configuration)
+        configurationPersister.saveConfiguration(newConfiguration)
         updateDisplay()
     }
 
     private fun updateDisplay() {
         val now = LocalTime(DateTimeZone.forID("Europe/Rome"))  // TODO the user should be able to change this
         val matrix = matrixDisplayer.createMatrix(now)
-        val matrixStr = matrixSerialiser.serialise(matrix, configuration)
+        val matrixStr = matrixSerialiser.serialise(matrix, configurationPersister.lastSavedConfiguration())
         serialConnection?.write(matrixStr.toByteArray(Charset.forName("UTF-8")))
     }
 
     fun stopPresenting() {
-        handler.removeCallbacks(triggerRunnable)
+        activity.unregisterReceiver(tickReceiver)
         serialConnection?.close()
     }
 }
