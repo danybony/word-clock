3f42a05066e26be1b69191f82576587aedc1c1f9
Daniele Bonaldo <dany.bony@gmail.com>
2017-10-20 23:49:38 +0200

    Update lights on Firebase database update

--- AndroidThings/things/src/main/java/net/bonysoft/wordclock/WordClockActivity.kt
+++ AndroidThings/things/src/main/java/net/bonysoft/wordclock/WordClockActivity.kt
@@ -4,8 +4,19 @@ import android.app.Activity
 import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.preference.PreferenceManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import net.bonysoft.wordclock.common.Configuration
+import timber.log.Timber
 
 class WordClockActivity : Activity() {
+
+    companion object {
+        private val FIREBASE_LIGHTS = "lights"
+        private val FIREBASE_LIGHTS_RGB = "spectrumRGB"
+    }
 
     private lateinit var configPresenter: ConfigPresenter
     private lateinit var arduinoConnectionPresenter: ArduinoConnectionPresenter
@@ -20,12 +31,26 @@ class WordClockActivity : Activity() {
                 MatrixSerialiser(),
                 configurationPersister
         )
+        val database = FirebaseDatabase.getInstance().reference.child(FIREBASE_LIGHTS)
+        database.addValueEventListener(lightChangedEventListener)
         configPresenter = ConfigPresenter(this,
                 getString(R.string.app_name),
                 getString(R.string.service_id),
                 configurationPersister,
                 arduinoConnectionPresenter::updateConfiguration
         )
+    }
+
+    private val lightChangedEventListener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val spectrumRGB = snapshot.child(FIREBASE_LIGHTS_RGB).getValue(Int::class.java) ?: 0
+            Timber.d("onDataChange (spectrum=$spectrumRGB)")
+            arduinoConnectionPresenter.updateConfiguration(Configuration(spectrumRGB))
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Timber.w("onCancelled", error.toException())
+        }
     }
 
     override fun onStart() {
